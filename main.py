d4cb29cc3481ccc64fdf3dc207607a9bf3a3dd5d
Jake Klingler <thearrow3456@gmail.com>
2013-02-28 21:52:01 -0500

    remove nasdaq and simplify

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ from matplotlib import dates
 
 #Financial Data
 sp500filename = 'sp500.csv'
-nasdaqfilename = 'nasdaq.csv'
 startdate = '20020101' #YYYYMMDD
 enddate = '20130220' #YYYYMMDD
 sp500mean = 0
 sp500max = 0
-nasdaqmean = 0
-nasdaqmax = 0
 
 #Neural Network
 INPUT = 20
@@ -31,11 +28,7 @@ def load_index(file):
         data.reverse()
         return data
     else:
-        data = []
-        if file == sp500filename:
-            data = ystockquote.get_historical_prices("%5EGSPC",startdate,enddate)
-        elif file == nasdaqfilename:
-            data = ystockquote.get_historical_prices("%5EIXIC",startdate,enddate)
+        data = ystockquote.get_historical_prices("%5EGSPC",startdate,enddate)
         data.pop(0)
         with open(file, 'wb') as f:
             writer = csv.writer(f)
@@ -44,37 +37,26 @@ def load_index(file):
         return data
 
 #data normalization functions {-1:1}
-def normalize(index, data):
+def normalize(data):
     mean = numpy.mean(list(float(d[-1]) for d in data))
     max = numpy.max(list(numpy.abs(float(d[-1])-mean) for d in data))
-    if index == 'sp500':
-        global sp500mean
-        sp500mean = mean
-        global sp500max
-        sp500max = max
-    elif index == 'nasdaq':
-        global nasdaqmean
-        nasdaqmean = mean
-        global nasdaqmax
-        nasdaqmax = max
+    global sp500mean, sp500max
+    sp500mean = mean
+    sp500max = max
     for i,val in enumerate(data):
         data[i][-1] = (float(val[-1])-mean)/max
 
-def un_normalize(index, data):
-    if index == 'sp500':
-        mean = sp500mean
-        max = sp500max
-    elif index == 'nasdaq':
-        mean = nasdaqmean
-        max = nasdaqmax
+def un_normalize(data):
     for i,val in enumerate(data):
-        data[i] = (float(val)*max)+mean
+        data[i] = (float(val)*sp500max)+sp500mean
 
 #Neural Net Functions
 def train(net, data):
     trainer = BackpropTrainer(net, data, learningrate=LRATE, momentum=0.9, weightdecay=0.0001)
-    for _ in range(ITERATIONS):
-        print trainer.train()
+    #for _ in range(ITERATIONS):
+    #    print trainer.train()
+    print "Training..."
+    trainer.trainUntilConvergence(maxEpochs=ITERATIONS,verbose=True)
 
 def create_training_data(input):
     data = SupervisedDataSet(INPUT,OUTPUT)
@@ -106,14 +88,9 @@ def get_output_dates(input):
 #Main program
 
 sp500 = load_index(sp500filename)
-normalize('sp500',sp500)
+normalize(sp500)
 spdates = list((dates.datestr2num(s[0]) for s in sp500))
 spvalues = list((s[-1] for s in sp500))
-
-nasdaq = load_index(nasdaqfilename)
-normalize('nasdaq',nasdaq)
-nasdates = list((dates.datestr2num(s[0]) for s in nasdaq))
-nasvalues = list((s[-1] for s in nasdaq))
 
 net = buildNetwork(INPUT,HIDDEN,OUTPUT)
 net.randomize()
@@ -122,15 +99,11 @@ data = create_training_data(spvalues)
 train(net,data)
 
 sp_predicted = get_output_vals(net, spvalues)
-print len(sp_predicted)
-print len(get_output_dates(sp500))
-un_normalize('sp500',spvalues)
-un_normalize('sp500',sp_predicted)
+un_normalize(spvalues)
+un_normalize(sp_predicted)
 
 pyplot.plot_date(spdates,spvalues,linestyle='solid',c='b',marker='None')
-#pyplot.plot_date(nasdates,nasvalues,linestyle='solid',c='g',marker='None')
 pyplot.plot_date(get_output_dates(sp500),sp_predicted,linestyle='solid',c='r',marker='None')
 pyplot.vlines(spdates[TRAINING+INPUT],1000,1600)
-
 
 pyplot.show()
