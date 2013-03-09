947c240807035a3ada06bf69fe0434e966434798
Jake Klingler <thearrow3456@gmail.com>
2013-03-09 12:01:31 -0500

    start over

--- datahandler.py
+++ datahandler.py
@@ -16,8 +16,6 @@ class DataHandler():
     data = []
     dates = []
     values = []
-    changes = []
-    changes2 = []
 
     #fetch financial data from file or yahoo API
     def load_index(self, ticker, startdate):
@@ -39,44 +37,3 @@ class DataHandler():
             self.data = data
         self.values = list((float(s[-1]) for s in self.data))
         self.dates = list((dates.datestr2num(s[0]) for s in self.data))
-        self.changes = self.createChanges(1)
-        self.changes2 = self.createChanges(2)
-        self.changes = self.normalize(self.changes, self.change_scaler)
-        self.changes2 = self.normalize(self.changes2, self.change2_scaler)
-        self.values = self.normalize(self.values, self.price_scaler)
-
-    def createChanges(self, days):
-        changes = []
-        for i in range(0, days):
-            changes.append(0)
-        for i in range(days, len(self.values)):
-            changes.append(self.values[i] - self.values[i - days])
-        return changes
-
-    #data normalization (0.2,0.8)
-    def normalize(self, input, scaler):
-        data = numpy.reshape(numpy.array(input), (len(input), 1))
-        scaler.fit_transform(data)
-        data = list(numpy.reshape(data, (1, len(data))).flatten())
-
-        for i in range(0, len(data)):
-            data[i] = data[i] * 0.6 + 0.2
-        return data
-
-    #not completed
-    def un_normalize(self):
-        self.values = numpy.reshape(numpy.array(self.values), (len(self.values), 1))
-        #self.scaler.inverse_transform(self.values)
-        self.values = list(numpy.reshape(self.values, (1, len(self.values))).flatten())
-
-    def get_dates(self):
-        return self.dates
-
-    def get_values(self):
-        return self.values
-
-    def get_changes(self):
-        return self.changes
-
-    def get_changes2(self):
-        return self.changes2
--- main.py
+++ main.py
@@ -32,79 +32,8 @@ def train(net, data):
     return trainer.trainUntilConvergence(maxEpochs=ITERATIONS)
 
 
-def create_training_data(input):
-    data = SupervisedDataSet(INPUT, OUTPUT)
-    count = 0
-    while count + DAYS + OUTPUT < TRAINING:
-        ins = []
-        for index in input['prices']:
-            ins.extend(index[count:count + DAYS])
-        for index in input['changes']:
-            ins.extend(index[count:count + 1])
-        for index in input['changes2']:
-            ins.extend(index[count:count + 1])
-        outs = input['prices'][0][count + DAYS + 1:count + DAYS + OUTPUT + 1]
-        data.addSample(ins, outs)
-        count += 1
-    return data
-
-
-def get_output_vals(net, input):
-    outputs = []
-    count = TRAINING
-    while count + DAYS + OUTPUT < TRAINING + TESTING:
-        ins = []
-        for index in input['prices']:
-            ins.extend(index[count:count + DAYS])
-        for index in input['changes']:
-            ins.extend(index[count:count + 1])
-        for index in input['changes2']:
-            ins.extend(index[count:count + 1])
-        outputs.append(net.activate(ins))
-        count += OUTPUT
-    realouts = []
-    for d in outputs:
-        realouts.append(list(d))
-    return list(itertools.chain(*realouts))
-
-
-def get_output_dates(dates):
-    return dates[TRAINING + DAYS:TRAINING + TESTING - OUTPUT]
-
-
-def absolute_errors(output, target):
-    errors = []
-    for i in range(0, len(output)):
-        errors.append((abs(target[i] - output[i])) / target[i])
-    return errors
-
-
 #Main program
-
-def assembleNetwork1():
-    # (INPUT : HIDDENSPLIT : HIDDENSPLIT : OUTPUT)
-    n = FeedForwardNetwork()
-    n.addInputModule(LinearLayer(INPUT, name="in"))
-    n.addModule(SigmoidLayer(HIDDEN / 4, name="h1"))
-    n.addModule(SigmoidLayer(HIDDEN / 4, name="h2"))
-    n.addModule(LinearLayer(HIDDEN / 4, name="h3"))
-    n.addModule(LinearLayer(HIDDEN / 4, name="h4"))
-    n.addModule(BiasUnit(name="bias"))
-    n.addOutputModule(SigmoidLayer(OUTPUT, name="out"))
-
-    n.addConnection(FullConnection(n['in'], n['h1']))
-    n.addConnection(FullConnection(n['in'], n['h3']))
-    n.addConnection(FullConnection(n['h1'], n['h2']))
-    n.addConnection(FullConnection(n['h3'], n['h4']))
-    n.addConnection(FullConnection(n['h4'], n['out']))
-    n.addConnection(FullConnection(n['h2'], n['out']))
-    n.addConnection(FullConnection(n['bias'], n['out']))
-    n.sortModules()
-    n = n.convertToFastNetwork()
-    return n
-
-
-def assembleNetwork2():
+def assembleNetwork():
     # (INPUT : HIDDEN : HIDDEN : OUTPUT)
     n = FeedForwardNetwork()
     n.addInputModule(LinearLayer(INPUT, name="in"))
@@ -115,99 +44,48 @@ def assembleNetwork2():
 
     n.addConnection(FullConnection(n['bias'], n['in']))
     n.addConnection(FullConnection(n['in'], n['h1']))
-    n.addConnection(FullConnection(n['in'], n['h2']))
+    n.addConnection(FullConnection(n['h1'], n['h2']))
     n.addConnection(FullConnection(n['h2'], n['out']))
     n.sortModules()
     n = n.convertToFastNetwork()
     return n
 
 #Feed-Forward:
-net = assembleNetwork2()
+net = assembleNetwork()
 net.randomize()
 
 #S&P 500
 sp500 = dh.DataHandler()
 sp500.load_index("%5EGSPC", startdate)
-spdates = sp500.get_dates()
-spvalues = sp500.get_values()
-spchanges = sp500.get_changes()
-spchanges2 = sp500.get_changes2()
 
 #NASDAQ COMPOSITE INDEX
 nasdaq = dh.DataHandler()
 nasdaq.load_index("%5EIXIC", startdate)
-nasvals = nasdaq.get_values()
-naschanges = nasdaq.get_changes()
-naschanges2 = nasdaq.get_changes2()
 
 #MAJOR MARKET INDEX
 tot = dh.DataHandler()
 tot.load_index("%5EXMI", startdate)
-totvals = tot.get_values()
-totchanges = tot.get_changes()
-totchanges2 = tot.get_changes2()
 
 #NYSE COMPOSITE INDEX
 nyse = dh.DataHandler()
 nyse.load_index("%5ENYA", startdate)
-nysevals = nyse.get_values()
-nysechanges = nyse.get_changes()
-nysechanges2 = nyse.get_changes2()
 
-vals = [spvalues, nasvals, totvals, nysevals]
-changes = [spchanges, naschanges, totchanges, nysechanges]
-changes2 = [spchanges2, naschanges2, totchanges2, nysechanges2]
-aggregated_input = {'prices': vals, 'changes': changes, 'changes2': changes2}
-data = create_training_data(aggregated_input)
-
-errors = train(net, data)
-
-sp_predicted = get_output_vals(net, aggregated_input)
-
-val = Validator()
-pred_errors = absolute_errors(sp_predicted, spvalues[TRAINING + DAYS + OUTPUT:TRAINING + TESTING])
-mse = val.MSE(sp_predicted, spvalues[TRAINING + DAYS + OUTPUT:TRAINING + TESTING])
-mape = (100.0 / len(pred_errors)) * numpy.sum(pred_errors)
 
 #Configure plots
-ENDTRAINING = TRAINING + DAYS
-predicted_dates = get_output_dates(spdates)
-pp.subplot2grid((3, 4), (0, 0), colspan=4)
-pp.plot_date(spdates, spvalues, linestyle='solid', c='black', marker='None', label="S&P500 Actual")
-pp.plot_date(spdates, nasvals, linestyle='dashed', c='g', marker='None', alpha=0.4)
-pp.plot_date(spdates, totvals, linestyle='dashed', c='b', marker='None', alpha=0.4)
-pp.plot_date(spdates, nysevals, linestyle='dashed', c='m', marker='None', alpha=0.4)
-pp.plot_date(predicted_dates, sp_predicted, linestyle='solid', c='r', marker='None', label="S&P500 Predicted")
-pp.vlines(spdates[ENDTRAINING], numpy.min(spvalues), numpy.max(spvalues))
-pp.ylabel("Normalized Indices")
-pp.text(spdates[ENDTRAINING - 350], numpy.max(spvalues) * .9, 'TRAINING')
-pp.text(spdates[ENDTRAINING + 50], numpy.max(spvalues) * .9, 'PREDICTION')
-pp.legend(fontsize='small', loc=4)
-pp.grid(True)
+# ENDTRAINING = TRAINING + DAYS
+# predicted_dates = get_output_dates(spdates)
+# pp.subplot2grid((3, 4), (0, 0), colspan=4)
+# pp.plot_date(spdates, spvalues, linestyle='solid', c='black', marker='None', label="S&P500 Actual")
+# pp.plot_date(spdates, nasvals, linestyle='dashed', c='g', marker='None', alpha=0.4)
+# pp.plot_date(spdates, totvals, linestyle='dashed', c='b', marker='None', alpha=0.4)
+# pp.plot_date(spdates, nysevals, linestyle='dashed', c='m', marker='None', alpha=0.4)
+# pp.plot_date(predicted_dates, sp_predicted, linestyle='solid', c='r', marker='None', label="S&P500 Predicted")
+# pp.vlines(spdates[ENDTRAINING], numpy.min(spvalues), numpy.max(spvalues))
+# pp.ylabel("Normalized Indices")
+# pp.text(spdates[ENDTRAINING - 350], numpy.max(spvalues) * .9, 'TRAINING')
+# pp.text(spdates[ENDTRAINING + 50], numpy.max(spvalues) * .9, 'PREDICTION')
+# pp.legend(fontsize='small', loc=4)
+# pp.grid(True)
 
-pp.subplot2grid((3, 4), (1, 0), colspan=4)
-pp.plot_date(spdates, spchanges, linestyle='none', c='black', marker='.', ms=2.0, label="1-Day Change", alpha=0.75)
-pp.plot_date(spdates, spchanges2, linestyle='none', c='green', marker='.', ms=2.0, label="2-Day Change", alpha=0.75)
-pp.ylabel("Normalized Change")
-pp.grid(True)
-pp.legend(fontsize='small', loc=4)
 
-pp.subplot2grid((3, 4), (2, 0))
-pp.plot(numpy.reshape(errors[0], (len(errors[0]), 1)), c='r')
-pp.xlabel("Training Epoch Number")
-pp.ylabel("Mean Squared Error", fontsize='small')
-pp.grid(True)
-
-pp.subplot2grid((3, 4), (2, 1))
-pp.plot(numpy.reshape(errors[1], (len(errors[1]), 1)), c='r')
-pp.xlabel("Validation Epoch Number")
-pp.grid(True)
-
-pp.subplot2grid((3, 4), (2, 2), colspan=2)
-pp.plot_date(predicted_dates, pred_errors, linestyle='solid', c='red', marker='None')
-pp.ylabel("Prediction Error", fontsize='small')
-errortext = 'Total MSE: %.4f\nTotal MAPE: %.2f' % (mse, mape) + "%"
-pp.text(predicted_dates[-1] - 250, numpy.max(pred_errors) * .8, errortext, bbox=dict(color='white', ec='black'))
-pp.grid(True)
-
-pp.show()
+# pp.show()
